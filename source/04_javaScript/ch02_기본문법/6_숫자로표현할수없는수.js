b014c429efb74ee1519558fda69cc8e63157475d
Whooahh <gndkrla2001@gmail.com>
2024-10-15 17:59:25 +0900

    javaScript

---
+++ source/04_javaScript/ch02_기본문법/6_숫자로표현할수없는수.js
@@ -0,0 +1,7 @@
+i = parseInt("");
+console.log('i타입은:', typeof(i), ', 값:' +i);
+a = 10/3;
+console.log('a타입은:', typeof(a), ', 값:' +a);
+console.log('i가 NaN인지 여부:', isNaN(i));
+console.log('a가 NaN인지 여부:', inNaN(a));
+console.log('a가 유한한 수냐');
